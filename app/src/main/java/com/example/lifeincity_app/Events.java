44862c8fc8a6fe863c19a9d21d18692ddc34f718
Dmitriy <dmitriy.folomkin@gmail.com>
2023-01-10 17:57:23 +0300

    2023-01-10 17:56 -m"insert/read data from firebase"

---
+++ app/src/main/java/com/example/lifeincity_app/Events.java
@@ -0,0 +1,5 @@
+package com.example.lifeincity_app;
+
+public class Events {
+
+}
--- app/src/main/java/com/example/lifeincity_app/MainActivity.java
+++ app/src/main/java/com/example/lifeincity_app/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.lifeincity_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText edName, edSecName, edEmail;
+    private DatabaseReference mDataBase;
+    private String USER_KEY = "User";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +25,29 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
-    private void init(){
+    private void init() {
         edName = findViewById(R.id.edName);
         edSecName = findViewById(R.id.edSecName);
         edEmail = findViewById(R.id.edEmail);
+        mDataBase = FirebaseDatabase.getInstance().getReference(USER_KEY);
     }
 
-    public void onClickSave(View view){
-
+    public void onClickSave(View view) {
+        String id = mDataBase.getKey();
+        String name = edName.getText().toString();
+        String sec_name = edSecName.getText().toString();
+        String email = edEmail.getText().toString();
+        User newUser = new User(id, name, sec_name, email);
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(sec_name) && !TextUtils.isEmpty(email)) {
+            mDataBase.push().setValue(newUser);
+            Toast.makeText(this, "Сохранено!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Пустое поле!", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    public void onClickRead(View view){
-
+    public void onClickRead(View view) {
+        Intent intent = new Intent(MainActivity.this, ReadActivity.class);
+        startActivity(intent);
     }
 }
